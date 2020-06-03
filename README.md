be3f2c6a3270000818f327eaa9922f37f18f748e
github username <github email address>
2020-06-03 16:38:11 -0700

    statrted the README

--- README.md
+++ README.md
@@ -1,24 +1,10 @@
-# README
+# Twitter/Facebook for Cats (Name to be Determined)
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+An app for cats to connect with each other and share their thoughts and feelings (because cats do have feelings).
 
-Things you may want to cover:
+This project was started with [Rails New](https://guides.rubyonrails.org/command_line.html).
 
-* Ruby version
+This project requires use of the [frontend repository](https://github.com/fosterv2/capstone-frontend)
 
-* System dependencies
 
-* Configuration
 
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-image_data = JSON.parse(RestClient.get('https://api.thecatapi.com/v1/images/search?limit=20'))
+image_data = JSON.parse(RestClient.get('https://api.thecatapi.com/v1/images/search?limit=20&mime_types=jpg,png'))
 url_list = image_data.map { |image| image["url"] }
-img_list = url_list.filter { |url| url.split(".").last != "gif" }
 
 10.times do
     name = Faker::Creature::Cat.name
     User.create(username: Faker::Internet.username(specifier: name), name: name, breed: Faker::Creature::Cat.breed,
-                password: Faker::Internet.password, owner_name: Faker::Name.first_name, img_url: img_list.pop)
+                password: Faker::Internet.password, owner_name: Faker::Name.first_name, url_list: img_list.pop)
 end
 
 
